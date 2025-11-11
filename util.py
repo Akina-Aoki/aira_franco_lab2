fc2ce86abb73aa4696783bdf0763cac9c7ff1af1
AiraFranco <adelosofranco@gmail.com>
2025-11-11 10:50:41 +0100

    added UML to readme. Changed intro explanation.

--- util.py
+++ util.py
@@ -10,6 +10,7 @@ def validate_number(value:int|float):
     """
     Validate that the given value is a number.
     Value : any type
+    
     Raises: TypeError if the value is a string type.
     """
     if not isinstance(value, Number):   
